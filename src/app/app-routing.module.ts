1ab4814af08ba7006adad64ed47a80527fadb4d9
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-01-23 12:39:58 -0300

    Se arma ruta default y se agrega canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserGuardService } from './services/user-guard.service';
+import { NoUserGuardService } from './services/no-user-guard.service';
+
+const defaultPathApp = '/';
 
 const routes: Routes = [
   { 
     path: '', 
     loadChildren: () => import('./modules/login-module/login.module').then(m => m.LoginModule), 
+    canActivate: [NoUserGuardService]
   },
+  {
+    path: '**', redirectTo: defaultPathApp
+  }
 ];
 
 @NgModule({
