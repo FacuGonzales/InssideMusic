08dd8e2ece76c5033aaa92e7a8f7c826bf9da8dd
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-01-23 12:35:22 -0300

    Se crean servicios de guard

---
+++ src/app/services/user-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuardService implements CanActivate{
+
+  constructor(private router: Router) { }
+
+  canActivate(){
+    let token = localStorage.getItem('token')
+
+    if(!token){
+      this.router.navigate(['/login']);
+      return false;
+    }
+    
+    return true;
+  }
+}
