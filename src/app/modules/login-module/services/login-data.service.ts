a75328a40db07465df1171f2b7d6b00322bfd68a
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-01-23 11:20:12 -0300

    Se arma metodo login

--- src/app/modules/login-module/services/login-data.service.ts
+++ src/app/modules/login-module/services/login-data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs' 
+import { ILoginDto } from '../models/login-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginDataService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  login(userLogin: ILoginDto): Observable<any>{
+    let url = 'https://accounts.spotify.com/api/token'
+
+    let params = JSON.stringify(userLogin);
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.post(url, params, {headers: headers});
+  } 
 }
