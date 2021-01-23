5cc7bc296e0c4896bcdcf9902fcd0bd3ffa82dad
FacuGonzales <facundogonzalesliendo@gmail.com>
2021-01-22 21:22:48 -0300

    Se agrega LoginRoutingModule al module

--- src/app/modules/login-module/login.module.ts
+++ src/app/modules/login-module/login.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { LoginRoutingModule } from './login-routing.module';
 import { LoginPageComponent } from './page/login-page.component';
 
 @NgModule({
@@ -8,7 +10,7 @@ import { LoginPageComponent } from './page/login-page.component';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-
+    LoginRoutingModule
   ],
   declarations: [
     LoginPageComponent
