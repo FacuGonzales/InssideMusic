ae32f6270f0b4bdb3cf1bc3406fa01f0241be46e
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-01-23 14:28:30 -0300

    Se arma logica de logOut

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-nav',
+  selector: 'nav-component',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logOut(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('token-type');
+
+    this.router.navigate(['/']);
   }
 
 }
