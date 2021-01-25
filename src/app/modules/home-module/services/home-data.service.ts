a0cbf3afd791cf8fcb5fd006970b97025a4166dd
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-01-25 12:02:07 -0300

    Se arma metodo para obtener top

--- src/app/modules/home-module/services/home-data.service.ts
+++ src/app/modules/home-module/services/home-data.service.ts
@@ -19,6 +19,25 @@ export class HomeDataService {
     return this.http.get(url, {headers: headers});
   }
 
-  
+  getPlayList(limit: number, market?: string): Observable<any>{
+    let idPlaylist = '37i9dQZEVXbMDoHDwVN2tF'
+    let tracksOffSet = 'tracks?offset=0'
+    let limite = `limit=${limit}`
+
+    let filters = `${idPlaylist}/${tracksOffSet}&${limite}`;
+
+    if(market) filters = `${idPlaylist}&market=${market}`;
+
+    let url = `https://api.spotify.com/v1/playlists/${filters}`
+
+    let token = localStorage.getItem('token') ? localStorage.getItem('token') : ''; 
+
+    let headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get(url, {headers: headers});
+  }
+
+
+
 }
 
