72921e0127ba2a1397c11be9983937fd621d9c7c
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-01-25 21:52:04 -0300

    Se arma metodo para obtener items

--- src/app/modules/search-module/services/search-data.service.ts
+++ src/app/modules/search-module/services/search-data.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchDataService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  searchAnItem(item: string, type: string): Observable<any>{
+
+    let filterItem = `?q=${item}`
+    let filterType = `&type=${type}`
+
+    let url = `https://api.spotify.com/v1/search${filterItem}${filterType}`
+
+    let token = localStorage.getItem('token') ? localStorage.getItem('token') : ''; 
+
+    let headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get(url, {headers: headers});
+  }
+
 }
