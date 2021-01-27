b2034244b1faaf93dc31fdd9c00cbcf9867bb074
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-01-27 13:00:06 -0300

    Se agrega variables

--- src/app/modules/information-module/page/information-page.component.ts
+++ src/app/modules/information-module/page/information-page.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { NavComponent } from 'src/app/shared/components';
+import { ArtistObject } from '../../home-module/models/artist-object';
+import { SimplifiedAlbumObject } from '../../home-module/models/simplified-album-object';
+import { TrackObject } from '../../home-module/models/track-object';
+import { InformationDataService } from '../services/information-data.service';
 
 @Component({
   selector: 'app-information-page',
   templateUrl: './information-page.component.html',
   styleUrls: ['./information-page.component.css']
 })
-export class InformationPageComponent implements OnInit {
+export class InformationPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  id: string = '';
+  type: string = '';
+
+  informationArtists: ArtistObject = {};
+  informationTracks: TrackObject = {};
+  informationAlbum: SimplifiedAlbumObject = {};
+
+  favorites: any[] = [];
+
+  subscribes: Subscription[] = [];
+
+  @ViewChild(NavComponent) navComponent!: NavComponent;
+
+  constructor(private infoData: InformationDataService,
+              private route: ActivatedRoute,
+              private alert: ToastrService,) { }
 
   ngOnInit(): void {
+    this.initParams();
+    this.getInformation();
   }
+
 
 }
