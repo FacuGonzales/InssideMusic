4581ea1587832fdf8e9ad09d9573449b9f465b6b
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-01-27 10:18:45 -0300

    Se crea metodo para obtener info de un artista

--- src/app/modules/information-module/services/information-data.service.ts
+++ src/app/modules/information-module/services/information-data.service.ts
@@ -1,9 +1,23 @@
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InformationDataService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getAnArtist(id:string): Observable<any>{
+
+    let url = `https://api.spotify.com/v1/artists/${id}`
+
+    let token = localStorage.getItem('token') ? localStorage.getItem('token') : ''; 
+
+    let headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get(url, {headers: headers});
+  }
 }
